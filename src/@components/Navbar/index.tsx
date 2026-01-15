d7980ef7617908a931c7fc1ba37f82b5ef8bae6c
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-15 23:56:28 +0800

    Add transition using  `motion`  on Navbar component

--- src/@components/Navbar/index.tsx
+++ src/@components/Navbar/index.tsx
@@ -1,20 +1,41 @@
 import ArrowUpRight from "../../assets/right-up.png";
 import NavLogo from "../../assets/logo.svg";
+import { motion } from "motion/react";
 
 export default function Navbar() {
     return (
         <>
-            <nav className="w-full lg:h-38 flex items-center justify-between lg:pt-10.75 lg:pl-8 lg:pr-8 lg:pb-11 bg-none">
+            <motion.nav
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+                className="w-full lg:h-38 flex items-center justify-between lg:pt-10.75 lg:pl-8 lg:pr-8 lg:pb-11 bg-none">
                 <div>
-                    <img src={NavLogo} alt="Image" className="w-28 h-9.75 object-center"/>
+                    <img src={NavLogo} alt="Image" className="w-28 h-9.75 object-center" />
                 </div>
 
                 <div>
                     <ul className="flex items-center gap-14">
-                        <li className="lg:text-base lg:mr-12 cursor-pointer hover:text-[#5D45FD]">About</li>
-                        <li className="lg:text-base lg:mr-12 cursor-pointer hover:text-[#5D45FD]">Works</li>
-                        <li className="lg:text-base lg:mr-12 cursor-pointer hover:text-[#5D45FD]">Services</li>
-                        <li className="lg:text-base lg:mr-12 cursor-pointer hover:text-[#5D45FD]">Contact</li>
+                        <motion.li
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.95 }}
+                            className="lg:text-base lg:mr-12 cursor-pointer transition-all ease-in-out duration-300 hover:text-[#5D45FD]">
+                            About
+                        </motion.li>
+                        <motion.li
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.95 }}
+                            className="lg:text-base lg:mr-12 cursor-pointer  transition-all ease-in-out duration-300 hover:text-[#5D45FD]">
+                            Works
+                        </motion.li>
+                        <motion.li
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.95 }}
+                            className="lg:text-base lg:mr-12 cursor-pointer transition-all ease-in-out duration-300 hover:text-[#5D45FD] ">Services</motion.li>
+                        <motion.li
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.95 }}
+                            className="lg:text-base lg:mr-12 cursor-pointer transition-all ease-in-out duration-300 hover:text-[#5D45FD] ">Contact</motion.li>
                     </ul>
                 </div>
 
@@ -24,7 +45,7 @@ export default function Navbar() {
                         <img src={ArrowUpRight} alt="Arrow Up" className="w-6 h-6" />
                     </button>
                 </div>
-            </nav>
+            </motion.nav>
         </>
     )
 }
