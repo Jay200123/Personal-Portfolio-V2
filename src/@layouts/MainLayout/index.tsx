b95be19fd8c416221973d4ac7b8b276d7e841b53
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-18 21:59:03 +0800

    Removed padding

--- src/@layouts/MainLayout/index.tsx
+++ src/@layouts/MainLayout/index.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
     return (
-        <main className="min-h-screen flex flex-col">
+        <main className="min-h-screen w-full flex flex-col">
             <Navbar />
-            <div className="lg:pt-8 lg:pb-8 md:pr-8 md:pl-8 md:py-8 md:pt-20 md:pb-20">
+            <div>
                 <Outlet />
             </div>
             <Footer />
